5190657a0e839aa3b3570f2dbfd0b3b3ac228625
b991 <wangbeiqi2@gmail.com>
2024-09-26 23:43:17 +0100

    fix buttons because classes are conflicting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html", "./src/**/*.html", "./index.html"],
+  content: ["./src/*.{html,js}", "./index.html"],
   theme: {
     extend: {},
   },
